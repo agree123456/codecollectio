0042269945a68a65d30cbc6a3c4c8ab44769f655
agree123456 <jh94398632@gmail.com>
2024-04-10 20:38:42 +0900

    import error clear

--- big3tasks/src/main/java/com/naver/contact/contactDao/Update.java
+++ big3tasks/src/main/java/com/naver/contact/contactDao/Update.java
@@ -1,10 +1,7 @@
 package com.naver.contact.contactDao;
-
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.naver.contact.contactutil.Dbconnection;
